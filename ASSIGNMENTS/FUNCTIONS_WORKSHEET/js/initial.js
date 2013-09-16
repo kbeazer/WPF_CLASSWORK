22ea52599dde70187e15fa863c87ea0594d9f969
kbeazer <kbeazer@fullsail.edu>
2013-09-16 13:41:06 -0400

    Functions Worksheet
    Functions Worksheet added to repository.

---
+++ ASSIGNMENTS/FUNCTIONS_WORKSHEET/js/initial.js
@@ -0,0 +1,24 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: KBmac
+ * Date: 9/16/13
+ * Time: 1:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+//Kareem K. Beazer 9/12/13 Functions Worksheet
+
+//Calculate the circumference of a circle.
+
+var radius=5;
+var circumference=Math.PI*2*radius;
+console.log("The circumference of the circle is "+circumference+"");
+
+//It takes 8.666666667 bee stings per pound to kill an animal. Calculate
+// how many bee stings are needed to kill an animal in a function
+
+var animal= "cow";
+var beeStings= 8.666666667;
+var weight= 25;
+var death= beeStings*weight;
+console.log("It takes "+death+" bee stings to kill a "+animal+"");
+
