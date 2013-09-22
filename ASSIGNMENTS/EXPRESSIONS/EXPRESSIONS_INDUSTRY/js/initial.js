9df3bc21113c35dea536ba8c56ab29a0100a2fc5
kbeazer <kbeazer@fullsail.edu>
2013-09-22 10:24:49 -0400

    Expressions
    Variable newArray was added to repository.

--- ASSIGNMENTS/EXPRESSIONS/EXPRESSIONS_INDUSTRY/js/initial.js
+++ ASSIGNMENTS/EXPRESSIONS/EXPRESSIONS_INDUSTRY/js/initial.js
@@ -9,5 +9,20 @@ var days= 365;
 var year= days;
 var productionRate= year/time;
 var client= prompt('Please enter the number of clients pending');
+var month= prompt('Please enter the digits of the current month');
+var newArray=[];
+newArray[1]= january;
+newArray[2]= february;
+newArray[3]= march;
+newArray[4]= april;
+newArray[5]= may;
+newArray[6]= june;
+newArray[7]= july;
+newArray[8]= august;
+newArray[9]= september;
+newArray[10]= october;
+newArray[11]= november;
+newArray[12]= december;
+
 
 
