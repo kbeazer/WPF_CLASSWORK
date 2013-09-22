94a102067b2fa55615120e18de147a30999e4895
kbeazer <kbeazer@fullsail.edu>
2013-09-22 12:22:09 -0400

    Expressions
    initial syntax added to Wacky folder

---
+++ ASSIGNMENTS/EXPRESSIONS/EXPRESSIONS_WACKY/js/initial.js
@@ -0,0 +1,17 @@
+//Kareem K. Beazer
+// 9/20/13
+// Expressions Wacky
+
+//***Potatoes in a Sack***
+
+var user= prompt('Please enter your name here')
+var sack= prompt('Please enter the number of potato bags');
+var potato= prompt('Please enter the number of potatoes in each bag');
+var newArray= [];
+newArray[0]= 'red';
+newArray[1]= 'blue';
+newArray[2]= 'green';
+newArray[3]= 'purple';
+newArray[4]= 'yellow';
+
+
