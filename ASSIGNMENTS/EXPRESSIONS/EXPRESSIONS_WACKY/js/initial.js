1c17c87e7492cb918eb8c38c84430b4b832555ce
kbeazer <kbeazer@fullsail.edu>
2013-09-22 13:14:16 -0400

    Expressions
    if and else statements were added to the Wacky folder

--- ASSIGNMENTS/EXPRESSIONS/EXPRESSIONS_WACKY/js/initial.js
+++ ASSIGNMENTS/EXPRESSIONS/EXPRESSIONS_WACKY/js/initial.js
@@ -7,11 +7,34 @@
 var user= prompt('Please enter your name here')
 var sack= prompt('Please enter the number of potato bags');
 var potato= prompt('Please enter the number of potatoes in each bag');
-var newArray= [];
+var totalAmount= potato*sack;
+var newArray= [totalAmount];
 newArray[0]= 'red';
 newArray[1]= 'blue';
 newArray[2]= 'green';
 newArray[3]= 'purple';
 newArray[4]= 'yellow';
+var result= '';
+if(totalAmount<20){
+    totalAmount= newArray[0];
+}else if(totalAmount<40){
+    totalAmount= newArray[1];
+}else if(totalAmount<60){
+    totalAmount= newArray[2];
+}else if(totalAmount<80){
+    totalAmount= newArray[3];
+}else{
+    totalAmount= newArray[4];
+}
+if(user==''){
+    result= 'Please enter your name to continue';
+}else if(sack==''){
+    result= 'Please enter the number of potato bags to continue';
+}else if(potato==''){
+    result= 'Please enter the number of potatoes in each bag to continue';
+}else{
+    result= ''
+}
+
 
 
