3f559f19c2803d7d3c0aeb8f0ec3046ce01ee5a8
kbeazer <kbeazer@fullsail.edu>
2013-09-22 23:39:59 -0400

    Conditionals
    variable workMonth was corrected in Industry js file

--- ASSIGNMENTS/CONDITIONALS/CONDITIONALS_INDUSTRY/js/initial.js
+++ ASSIGNMENTS/CONDITIONALS/CONDITIONALS_INDUSTRY/js/initial.js
@@ -4,19 +4,20 @@
 
 //***Pay calculator***
 var salary= prompt('Enter your desired salary here');
-var workHours= 8;
+var workDay= 8;
 var workWeek= 5;
-var workMonth= 30;
+var workMonth= 4;
 var workYear= 12;
-var totalHours= workHours*workWeek*workMonth*workYear;
+var totalHours= workDay*workWeek*workMonth*workYear;
 var hourlyRate= salary/totalHours;
 var roundedPay= Math.floor(hourlyRate)
+console.log(salary)
 if(salary==''){
     console.log('Please enter your desired salary to continue');
 }else if(salary<14400){
     console.log('Please enter a minimum value of 20000')
 }else{
-    console.log('Based on your entered salary, you need a job that pays a minimum of '+roundedPay+'/hour')
+    console.log('Based on your entered salary, you need a job that pays a minimum of $'+roundedPay+'/hour')
 }
 
 
