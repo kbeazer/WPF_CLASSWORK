f8fc3a8c61666f6a09c2f2a219dde511b652be89
kbeazer <kbeazer@fullsail.edu>
2013-09-22 23:27:06 -0400

    Conditionals
    roundedPay variable was added to Industry js file

--- ASSIGNMENTS/CONDITIONALS/CONDITIONALS_INDUSTRY/js/initial.js
+++ ASSIGNMENTS/CONDITIONALS/CONDITIONALS_INDUSTRY/js/initial.js
@@ -9,14 +9,14 @@ var workWeek= 5;
 var workMonth= 30;
 var workYear= 12;
 var totalHours= workHours*workWeek*workMonth*workYear;
-console.log(totalHours)
 var hourlyRate= salary/totalHours;
+var roundedPay= Math.floor(hourlyRate)
 if(salary==''){
     console.log('Please enter your desired salary to continue');
 }else if(salary<14400){
     console.log('Please enter a minimum value of 20000')
 }else{
-    console.lgo('Based on your entered salary, you need a job that pays a minimum of '+hourlyRate+'/hour')
+    console.log('Based on your entered salary, you need a job that pays a minimum of '+hourlyRate+'/hour')
 }
 
 
