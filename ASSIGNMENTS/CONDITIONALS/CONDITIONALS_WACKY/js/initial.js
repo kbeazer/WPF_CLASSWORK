ce90920d700181358d34c71d1fe886f58d5f431f
kbeazer <kbeazer@fullsail.edu>
2013-09-23 11:48:05 -0400

    Conditionals Wacky
    comment was added for quarter variable in Wacky js file

--- ASSIGNMENTS/CONDITIONALS/CONDITIONALS_WACKY/js/initial.js
+++ ASSIGNMENTS/CONDITIONALS/CONDITIONALS_WACKY/js/initial.js
@@ -4,6 +4,7 @@
 
 //***Xbox 1 or Playstation 4***
 
+// This line declares quarter as a variable
 var quarter;
 var heads= 1;
 var tails= 4;
@@ -15,17 +16,18 @@ var consoleB= 'Xbox One';
 
 function coinToss(){
     var flipIt= Math.random()*5;
-    var callIt= Math.floor(flipIt)
+    var callIt= Math.floor(flipIt);
     if(callIt>3){
         quarter= tails;
 }else if(callIt<3){
         quarter= heads;
     }else{
-        return 'The coin landed directly on it'+'s'+ 'side......rare occasion, fip again (and buy a lottery ticket today)'
+        return 'The coin landed directly on its side......rare occasion, fip again (and buy a lottery ticket today)'
     }if(quarter== tails){
         ps4= true;
         console.log('The Mighty Coin has spoken.......'+consoleA+' is the winner of the coin toss!!!')
     }else{
         return 'The Mighty Coin has spoken.......'+consoleB+' is the winner of the coin toss!!!'
     }
-}console.log(coinToss())
+}console.log(coinToss());
+
