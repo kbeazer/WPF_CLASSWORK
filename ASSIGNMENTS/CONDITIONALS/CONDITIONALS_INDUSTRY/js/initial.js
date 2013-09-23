ea664824912d0a671fa091096842eb7199856b6d
kbeazer <kbeazer@fullsail.edu>
2013-09-22 23:23:13 -0400

    Conditionals
    if and else statements were added to Industry js file

--- ASSIGNMENTS/CONDITIONALS/CONDITIONALS_INDUSTRY/js/initial.js
+++ ASSIGNMENTS/CONDITIONALS/CONDITIONALS_INDUSTRY/js/initial.js
@@ -9,6 +9,15 @@ var workWeek= 5;
 var workMonth= 30;
 var workYear= 12;
 var totalHours= workHours*workWeek*workMonth*workYear;
+console.log(totalHours)
+var hourlyRate= salary/totalHours;
+if(salary==''){
+    console.log('Please enter your desired salary to continue');
+}else if(salary<14400){
+    console.log('Please enter a minimum value of 20000')
+}else{
+    console.lgo('Based on your entered salary, you need a job that pays a minimum of '+hourlyRate+'/hour')
+}
 
 
 
