5b1554b2cd90faf5baa66eda28be9ccd3362dc3c
kbeazer <kbeazer@fullsail.edu>
2013-09-27 11:45:46 -0400

    Functions Industry
    Fuctions Industry folder was added to repository

---
+++ ASSIGNMENTS/FUNCTIONS/FUNCTIONS_INDUSTRY/js/initial.js
@@ -0,0 +1,4 @@
+// Kareem K. Beazer
+// 9/23/13
+// Functions Industry
+
---
+++ ASSIGNMENTS/FUNCTIONS/FUNCTIONS_PERSONAL/js/initial.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: KBmac
+ * Date: 9/27/13
+ * Time: 11:41 AM
+ * To change this template use File | Settings | File Templates.
+ */
