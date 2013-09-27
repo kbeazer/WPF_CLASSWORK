ebf6ecf6fc0045e8cef3d72d08f8befabcd5c9a9
kbeazer <kbeazer@fullsail.edu>
2013-09-27 17:23:46 -0400

    Functions Personal
    monthsOfYear array was added to Personal js file

--- ASSIGNMENTS/FUNCTIONS/FUNCTIONS_PERSONAL/js/initial.js
+++ ASSIGNMENTS/FUNCTIONS/FUNCTIONS_PERSONAL/js/initial.js
@@ -8,6 +8,19 @@ var currentYear= 2013;
 
 function idChecker(){
     var userAge= currentYear-yearBorn;
+    var monthsOfYear= [];
+    monthsOfYear[0]= 'January';
+    monthsOfYear[1]= 'February';
+    monthsOfYear[2]= 'March';
+    monthsOfYear[3]= 'April';
+    monthsOfYear[4]= 'May';
+    monthsOfYear[5]= 'June';
+    monthsOfYear[6]= 'July';
+    monthsOfYear[7]= 'August';
+    monthsOfYear[8]= 'September';
+    monthsOfYear[9]= 'October';
+    monthsOfYear[10]= 'November';
+    monthsOfYear[11]= 'December';
     if(monthBorn==''){
         return 'Please enter the month you were born to continue'
 }else if(monthBorn>12){
