10f508efbb7f3edef0d388e71d7e738ed786c292
kbeazer <kbeazer@fullsail.edu>
2013-09-27 17:39:39 -0400

    Functions Wacky
    Fuctions Wacky folder was added to repository

--- ASSIGNMENTS/FUNCTIONS/FUNCTIONS_WACKY/js/initial.js
+++ ASSIGNMENTS/FUNCTIONS/FUNCTIONS_WACKY/js/initial.js
@@ -1,7 +1,4 @@
-/**
- * Created with JetBrains WebStorm.
- * User: KBmac
- * Date: 9/27/13
- * Time: 11:41 AM
- * To change this template use File | Settings | File Templates.
- */
+// Kareem K. Beazer
+// 9/23/13
+// Functions Wacky
+
