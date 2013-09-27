33454a226a579b6f66d6df985244d691eb886841
kbeazer <kbeazer@fullsail.edu>
2013-09-27 11:53:57 -0400

    Functions Industry
    variables were established in Functions Industry js file

--- ASSIGNMENTS/FUNCTIONS/FUNCTIONS_INDUSTRY/js/initial.js
+++ ASSIGNMENTS/FUNCTIONS/FUNCTIONS_INDUSTRY/js/initial.js
@@ -2,3 +2,11 @@
 // 9/23/13
 // Functions Industry
 
+var actualCost= 25;
+var salesTax= .07;
+var taxAmount= actualCost*salesTax;
+var totalCost= actualCost+taxAmount;
+
+function roundDown(){
+
+}
