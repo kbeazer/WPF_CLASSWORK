3e209304dedc71d8e234ecc0212d41732f641cef
kbeazer <kbeazer@fullsail.edu>
2013-09-27 12:11:30 -0400

    Functions Industry
    if statement was addes to Functions Industry js file

--- ASSIGNMENTS/FUNCTIONS/FUNCTIONS_INDUSTRY/js/initial.js
+++ ASSIGNMENTS/FUNCTIONS/FUNCTIONS_INDUSTRY/js/initial.js
@@ -8,5 +8,12 @@ var taxAmount= actualCost*salesTax;
 var totalCost= actualCost+taxAmount;
 
 function roundDown(){
+    var flatDollar= Math.floor(totalCost);
+    if(actualCost<0){
+        totalCost= flatDollar;
+}else if(actualCost==''){
+        return 'Please enter the dollar amount to continue';
+}else{
 
+}
 }
