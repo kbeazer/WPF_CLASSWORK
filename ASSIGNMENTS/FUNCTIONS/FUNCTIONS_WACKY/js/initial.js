5e5c6844508fa8f352be00d86fb6a77d7e1c42ac
kbeazer <kbeazer@fullsail.edu>
2013-09-27 23:47:56 -0400

    Conditionals Wacky
    Conditionals Wacky added to repository

--- ASSIGNMENTS/FUNCTIONS/FUNCTIONS_WACKY/js/initial.js
+++ ASSIGNMENTS/FUNCTIONS/FUNCTIONS_WACKY/js/initial.js
@@ -2,3 +2,20 @@
 // 9/23/13
 // Functions Wacky
 
+var lawnMower= 25;
+var tricycle= 50;
+var loveSeat= 75;
+var airPlane= 100;
+function ranVariable(optionA,optionB,optionC,optionD){
+    var wheelSpin= Math.random()*100;
+    if(wheelSpin<25){
+        return optionA;
+    }else if(wheelSpin<50){
+        return optionB;
+    }else if(wheelSpin<75){
+        return optionC;
+    }else{
+        return optionD;
+    }
+
+}
